be8f25728e4e893d24aaac9990d630b2b53fdbb5
Marlena Śliwińska <64863486+marlena-sliwinska@users.noreply.github.com>
2023-04-29 11:38:11 +0200

    init layout (#1)

---
+++ src/layout/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import { chakra, Flex } from '@chakra-ui/react';
+
+import { Navigation } from 'layout/Navigation';
+import { Footer } from 'layout/Footer';
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <Flex minH="100vh" flexFlow="column">
+      <Navigation />
+      <chakra.main flexGrow={1}>{children}</chakra.main>
+      <Footer />
+    </Flex>
+  );
+};
+
+export default Layout;
