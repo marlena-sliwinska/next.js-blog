eb6cb94752dc97c9bcc262bc0645edbffb72e52c
Marlena Śliwińska <marlenasliwinska@gmail.com>
2023-08-24 14:02:17 +0200

    head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AppProps } from 'next/app';
 
@@ -7,6 +8,10 @@ import theme from '../theme';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>My blog title</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
