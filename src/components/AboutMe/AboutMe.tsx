5ed8001ac24aaf26a208fc8056673aaae4137a24
Marlena Śliwińska <marlenasliwinska@gmail.com>
2023-08-26 12:38:10 +0200

    about me image

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,28 +1,36 @@
-import { Stack, Text, Image } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Stack, Text, chakra } from '@chakra-ui/react';
 
-const AboutMe = () => (
-  <Stack
-    spacing={4}
-    p={2}
-    borderWidth={{ base: '1px 0 0 0', md: '1px' }}
-    borderColor="gray.200"
-    alignSelf="center"
-    alignItems={{ base: 'center', md: 'flex-start' }}
-  >
-    <Text fontWeight="bold">ABOUT ME</Text>
-    <Image
-      width="220px"
-      height="160px"
-      src={'https://loremflickr.com/220/160/paris,girl'}
-      alt="about me image"
-    />
-    <Text maxW={450} textAlign="justify" textIndent="15px">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium leo
-      urna. Vestibulum fermentum iaculis libero a egestas. Aenean ac finibus orci.
-      Etiam lacinia orci quis purus auctor facilisis. Nulla facilisi. Nulla in nibh
-      quam.
-    </Text>
-  </Stack>
-);
+const AboutMeImage = chakra(Image, {
+  shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop),
+});
 
+const AboutMe = () => {
+  return (
+    <Stack
+      spacing={4}
+      p={2}
+      borderWidth={{ base: '1px 0 0 0', md: '1px' }}
+      borderColor="gray.200"
+      alignSelf="center"
+      alignItems={{ base: 'center' }}
+    >
+      <Text fontWeight="bold">ABOUT ME</Text>
+      <AboutMeImage
+        objectFit="contain"
+        width="151"
+        height="259"
+        rounded={'3xl'}
+        src={'/images/basic/aboutMe/about_me.jpg'}
+        alt="about me image"
+      />
+      <Text maxW={450} textAlign="justify" textIndent="15px">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus pretium
+        leo urna. Vestibulum fermentum iaculis libero a egestas. Aenean ac finibus
+        orci. Etiam lacinia orci quis purus auctor facilisis. Nulla facilisi. Nulla
+        in nibh quam.
+      </Text>
+    </Stack>
+  );
+};
 export default AboutMe;
